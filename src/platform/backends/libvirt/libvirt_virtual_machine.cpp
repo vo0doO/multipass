2cda1d80bb17ae989132639a0d2bf8c12bd85dab
Gerry Boland <gerboland@gmail.com>
2019-03-11 16:35:55 +0000

    Missed this

--- src/platform/backends/libvirt/libvirt_virtual_machine.cpp
+++ src/platform/backends/libvirt/libvirt_virtual_machine.cpp
@@ -125,7 +125,6 @@ auto generate_xml_config_for(const mp::VirtualMachineDescription& desc, const st
 
     auto qemu_path = fmt::format("/usr/bin/qemu-system-{}", arch);
 
-    auto snap = qgetenv("SNAP");
     if (mu::is_snap_confined())
     {
         auto snap_path = QDir(mu::snap_dir());
